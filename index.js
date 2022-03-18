f5e0fa92f358723ec896ed7e166c32bdbbf8572c
JoshLutrick <joshslutrick@gmail.com>
2022-03-18 01:47:06 -0700

    added transition between x and bars

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const navElements = {
 function toggleMenu(){
     if(navElements.navMenu.classList.contains('navbarmenu__close')){
         navElements.navMenu.classList.remove('navbarmenu__close')
+        navElements.navIcon.classList.remove('fa-bars')
+        navElements.navIcon.classList.add('fa-xmark')
     } else if (!navElements.navMenu.classList.contains('navbarmenu__close')){
         navElements.navMenu.classList.add('navbarmenu__close')
+        navElements.navIcon.classList.add('fa-bars')
+        navElements.navIcon.classList.remove('fa-xmark')
     }
 }
 
